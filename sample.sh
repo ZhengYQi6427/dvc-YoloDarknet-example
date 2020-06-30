fc25c9927541e0c72db4f30839b22809274a39a9
Jocey.Z <62313047+ZhengYQi6427@users.noreply.github.com>
2020-06-30 01:55:42 -0400

    Add files via upload

--- sample.sh
+++ sample.sh
@@ -3,14 +3,16 @@
 
 ##### CONSTANT
 
-echo -n "Enter your github repository url > "
-read github_link
+#echo -n "Enter your github repository url > "
+#read github_link
+echo -n "Enter your Github repository name > "
+read repo_name
 #echo -n "Enter your Synology video file path > "
 #read video_link
 NAS_account="ra_yz3622"
 NAS_password="summer"
-echo -n "Enter yout target directory > "
-read dir
+#echo -n "Enter yout target directory > "
+#read dir
 
 ##### FUNCTIONS
 
@@ -20,17 +22,24 @@ get_videos()
    mv $(pwd)/DVC-data/images $(pwd)
    mv $(pwd)/DVC-data/annotations $(pwd)
    rm -rf DVC-data
+   
+   dvc add $(pwd)/images
+   dvc add $(pwd)/annotations
+   git add images.dvc
+   git add annotations.dvc
+   git commit -m 'add data archive'
 }
 
 get_code()
 {
    git clone "$github_link"
    rm -rf .git
-   echo -n "Enter your repository name > "
-   read repo_name
    cd $repo_name
    sed -i 's+$(pwd)+g' json2voc.py
    cd ..
+
+   git add $(pwd)/$repo_name
+   git commit -m 'add code'
 }
 
 init()
@@ -68,10 +77,38 @@ init()
    dvc init
 }
 
+preprocess(){
+    echo -n "Begin preprocessing? [y/n] "
+    read
+    if [ "$REPLY" = "y" ]; then
+        echo -n "How many preprocessing scripts need to run > "
+        read file_num
+
+        cd $repo_name
+        sudo apt install python-pip
+        pip install -r requirements.txt
+        echo "$(pwd)"
+
+        i=0
+        until [ $i -ge $file_num ];
+        do
+            echo -n "Enter file $i name > "
+            read file_name
+            if [ !-e $file_name ]; then
+                echo "ERROR: File $varname doesn't exist"
+                exit 0
+            fi
+            python $file_name
+            i=$((i + 1))
+        done
+    fi
+}
+
 ##### MAIN
 
 # init
 # rm -rf .dvc
 # rm -rf .git
 # get_videos
-get_code
+# get_code
+preprocess
